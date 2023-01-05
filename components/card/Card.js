e66c1a9a3b248df9309bc6aa50b8dea8963039d9
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-05 18:46:22 +0300

    adding serchbar component

--- components/card/Card.js
+++ components/card/Card.js
@@ -5,7 +5,7 @@ const Card = ({pokemon}) => {
   return (
     <div   className={'flex flex-col items-center gap-5 hover:scale-110 duration-200 ease-in-out'}>
 
-    <Image className='h-[15rem] w-[14rem]' alt={pokemon.name}  src={'https://jherr-pokemon.s3.us-west-1.amazonaws.com/'+pokemon.image}  width={200} height={500}
+    <Image className='lg:h-[15rem] lg:w-[14rem]' alt={pokemon.name}  src={'https://jherr-pokemon.s3.us-west-1.amazonaws.com/'+pokemon.image}  width={200} height={500}
     />
     <p className='text-xl '>{pokemon.name}</p>
     
