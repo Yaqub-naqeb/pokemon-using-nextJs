fdc8dc55ea5f903420a62f46b27da61243c2371b
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-09 16:31:22 +0300

    connecting firebase with our project

--- pages/form/LogIn.js
+++ pages/form/LogIn.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { auth } from '../firebase'
 import {signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+import {useAuthState} from 'react-firebase-hooks/auth'
+
 
 const LogIn = () => {
+  const [user,setUser]=useAuthState(auth);
+
+  useEffect(()=>{
+    console.log(user);
+  },[user])
 
 const googleAuth=new GoogleAuthProvider();
+const login=async()=>{
+  const result=await signInWithPopup(auth,googleAuth);
 
+}
   return (
     <div>
-    <button></button>
+    <button onClick={login}>login</button>
+
+   {user&&  <h1>welcome {user.displayName}</h1>}
+
+<button onClick={()=>auth.signOut()}>logout</button>
+
     </div>
   )
 }
