5c441adca1494dacfe3d9b495ad033fd388243aa
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-10 17:08:34 +0300

    signIn with email and password

--- pages/form/LogIn.js
+++ pages/form/LogIn.js
@@ -4,22 +4,25 @@ import { auth } from '../firebase'
 import {signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import Button from '../../components/Button'
-
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from 'next/router'
+import NotFound from '../404'
 
 const LogIn = () => {
+
+const router=useRouter();
+
+
+
   const [email,setEmail]=useState('');
 const [password,setPassword]=useState('');
   const [user,setUser]=useAuthState(auth);
 
-  useEffect(()=>{
-    console.log(user);
-  },[user])
 
-const googleAuth=new GoogleAuthProvider();
-const login=async()=>{
-  const result=await signInWithPopup(auth,googleAuth);
 
-}
+  
+
+ 
 
 
 
@@ -29,46 +32,67 @@ const login=async()=>{
 const submitHandler=(e)=>{
   e.preventDefault();
 
-  console.log(firstName+lastName+email+confirmPassword);
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      router.push('/')
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
 
 setEmail('')
 setPassword('')
+}
+
+// login with popUp
+const googleAuth=new GoogleAuthProvider();
+const login=async()=>{
+  const result=await signInWithPopup(auth,googleAuth);
+  router.push('/');
 
 }
 
 
   return (
-    <div className='px-[10rem] lg:translate-x-0 md:translate-x-0 -translate-x-10 bg-slate-100 relative  shadow-sm rounded-md lg:mx-[30rem] md:mx-[10rem]  z-20 '>
+   <>
+   {user ?<NotFound/>: <div className='px-[10rem] lg:translate-x-0 md:translate-x-0 -translate-x-10 bg-slate-100 relative  shadow-sm rounded-md lg:mx-[30rem] md:mx-[10rem]  z-20 '>
 
 
 
-     <form onSubmit={submitHandler}
-     
-     className='py-10 z-50  flex items-center flex-col'>
+<form onSubmit={submitHandler}
+
+className='py-10 z-50  flex items-center flex-col'>
 
 <h1 className={`mb-10  font-bold w-[4rem]`}>LOG IN</h1>
 
 
 <div className='flex items-center flex-col gap-[1rem]'>
 <input 
-  className='w-[15rem]
-  border-[#0000b] border-b-2
-  outline-none
-  rounded-md p-1
-  shadow-sm
-  '
-  type="email" placeholder='Email' onChange={e=>setEmail(e.target.value)} />
+className='w-[15rem]
+border-[#0000b] border-b-2
+outline-none
+rounded-md p-1
+shadow-sm
+'
+type="email" placeholder='Email' onChange={e=>setEmail(e.target.value)} />
 
-  <input
-  className='w-[15rem]
-  border-[#0000b] border-b-2
-  outline-none
-  rounded-md p-1
-  shadow-sm
-  '
-  type="password" placeholder='Password' onChange={e=>setPassword(e.target.value)} />
+<input
+className='w-[15rem]
+border-[#0000b] border-b-2
+outline-none
+rounded-md p-1
+shadow-sm
+'
+type="password" placeholder='Password' onChange={e=>setPassword(e.target.value)} />
 
-  
+
 
 <div className='h-[.01rem] bg-black w-[17rem] md:w-[18rem] lg:w-[20rem]'></div>
 
@@ -84,9 +108,10 @@ setPassword('')
 
 <Button  name={'Submit'}/>
 
-     </form>
-   
-    </div>
+</form>
+
+</div>}
+   </>
   )
 }
 
--- pages/form/SignUp.js
+++ pages/form/SignUp.js
@@ -5,12 +5,15 @@ import { auth } from '../firebase'
 import {signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import { useRouter } from 'next/router';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import NotFound from '../404';
 
 const inter = Inter({ subsets: ['latin'] })
 
 const SignUp = () => {
-  const router=useRouter();
 
+const auth = getAuth();
+const router=useRouter();
 const [firstName,setFirstName]=useState('');
 const [lastName,setLastName]=useState('');
 const [email,setEmail]=useState('');
@@ -18,10 +21,25 @@ const [password,setPassword]=useState('');
 const [confirmPassword,setConfirmPassword]=useState('');
 
 
+
 const submitHandler=(e)=>{
   e.preventDefault();
 
-  console.log(firstName+lastName+email+confirmPassword);
+
+  createUserWithEmailAndPassword(auth, email, password,firstName)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  router.push('/')
   
 setFirstName('')
 setLastName('')
@@ -31,9 +49,6 @@ setConfirmPassword('')
 }
 // 
 
-
-
-
   const [user,setUser]=useAuthState(auth);
 
   useEffect(()=>{
@@ -41,6 +56,7 @@ setConfirmPassword('')
   },[user])
 
 const googleAuth=new GoogleAuthProvider();
+
 const login=async()=>{
   const result=await signInWithPopup(auth,googleAuth);
   router.push('/');
@@ -50,67 +66,74 @@ const login=async()=>{
 
 
   return (
-    <div className='px-[10rem] lg:translate-x-0 md:translate-x-0 -translate-x-10 bg-slate-100 relative  shadow-sm rounded-md lg:mx-[30rem] md:mx-[10rem]  z-20 '>
+
+    <>
+    {user?<NotFound/>:<div className='px-[10rem] lg:translate-x-0 md:translate-x-0 -translate-x-10 bg-slate-100 relative  shadow-sm rounded-md lg:mx-[30rem] md:mx-[10rem]  z-20 '>
 
 
 
-     <form onSubmit={submitHandler}
-     
-     className='py-10 z-50  flex items-center flex-col'>
+<form onSubmit={submitHandler}
+
+className='py-10 z-50  flex items-center flex-col'>
 
 <h1 className={`mb-10 ${inter} font-bold w-[4rem]`}>SIGN UP</h1>
 
 
 <div className='flex items-center flex-col gap-[1rem]'>
-  <input  className='w-[15rem]
-  border-[#0000b] border-b-2
-  outline-none
-  rounded-md p-1
-  shadow-sm
-  '
-  
-  type="text" placeholder='First name' onChange={e=>setFirstName(e.target.value)} />
+<input value={firstName} className='w-[15rem]
+border-[#0000b] border-b-2
+outline-none
+rounded-md p-1
+shadow-sm
+'
 
-  <input
-  className='w-[15rem]
-  border-[#0000b] border-b-2
-  outline-none
-  rounded-md p-1
-  shadow-sm
-  '
-  type="text" placeholder='Last name' onChange={e=>setLastName(e.target.value)} />
+type="text" placeholder='First name' onChange={e=>setFirstName(e.target.value)} />
 
-  <input 
-  className='w-[15rem]
-  border-[#0000b] border-b-2
-  outline-none
-  rounded-md p-1
-  shadow-sm
-  '
-  type="email" placeholder='Email' onChange={e=>setEmail(e.target.value)} />
+<input
+value={lastName}
+className='w-[15rem]
+border-[#0000b] border-b-2
+outline-none
+rounded-md p-1
+shadow-sm
+'
+type="text" placeholder='Last name' onChange={e=>setLastName(e.target.value)} />
 
-  <input
-  className='w-[15rem]
-  border-[#0000b] border-b-2
-  outline-none
-  rounded-md p-1
-  shadow-sm
-  '
-  type="password" placeholder='Password' onChange={e=>setPassword(e.target.value)} />
+<input 
+value={email}
+className='w-[15rem]
+border-[#0000b] border-b-2
+outline-none
+rounded-md p-1
+shadow-sm
+'
+type="email" placeholder='Email' onChange={e=>setEmail(e.target.value)} />
 
-  <input
-  className='w-[15rem]
-  border-[#0000b] border-b-2
-  outline-none
-  rounded-md p-1
-  shadow-sm
-  '
-  type="password" placeholder='Confirm Password' onChange={e=>setConfirmPassword(e.target.value)} />
+<input
+value={password}
+className='w-[15rem]
+border-[#0000b] border-b-2
+outline-none
+rounded-md p-1
+shadow-sm
+'
+type="password" placeholder='Password' onChange={e=>setPassword(e.target.value)} />
+
+<input
+value={confirmPassword}
+className='w-[15rem]
+border-[#0000b] border-b-2
+outline-none
+rounded-md p-1
+shadow-sm
+'
+type="password" placeholder='Confirm Password' onChange={e=>setConfirmPassword(e.target.value)} />
 
 <div className='h-[.01rem] bg-black w-[17rem] md:w-[18rem] lg:w-[20rem]'></div>
 
 <p>or</p>
 <button onClick={login}>Login with Google</button>
+{/* <button onClick={login2}>Login with facebook</button> */}
 
 
 
@@ -122,9 +145,10 @@ const login=async()=>{
 {/* <button type='submit'>Submit</button> */}
 <Button  name={'Submit'}/>
 
-     </form>
-   
-    </div>
+</form>
+
+</div>}
+    </>
   )
 }
 
